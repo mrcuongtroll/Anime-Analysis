d7b8ad462ec25942e216315066cdd4343bafdbbe
mrcuongtroll <38219186+mrcuongtroll@users.noreply.github.com>
2021-10-30 14:28:23 +0700

    added spider to scrape from anisearch.com

---
+++ anime/anime/items.py
@@ -0,0 +1,25 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class AnimeItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    media_type = scrapy.Field()
+    num_episodes = scrapy.Field()
+    duration = scrapy.Field()
+    status = scrapy.Field()
+    start_date = scrapy.Field()
+    end_date = scrapy.Field()
+    source = scrapy.Field()
+    studio = scrapy.Field()
+    target_group = scrapy.Field()
+    genres = scrapy.Field()
+    tags = scrapy.Field()
+    score = scrapy.Field()
+    rank = scrapy.Field()
+    pass
